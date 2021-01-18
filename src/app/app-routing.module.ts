3f647d0f3d11d8443ac45d4ab325777630e6a153
josue(linux) <josuepalaci@gmail.com>
2021-01-17 19:45:47 -0600

    Ejemplo de Ionic import y export Excel

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,14 +3,55 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   {
-    path: 'home',
-    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
+    path: "home",
+    loadChildren: () =>
+      import("./home/home.module").then((m) => m.HomePageModule),
   },
   {
-    path: '',
-    redirectTo: 'home',
-    pathMatch: 'full'
+    path: "",
+    redirectTo: "formularios",
+    pathMatch: "full",
   },
+  {
+    path: "movies",
+    children: [
+      {
+        path: '',
+        loadChildren: () => import("./movies/movies.module").then((m) => m.MoviesPageModule),
+      },
+      {
+        path: ':movieId',
+        loadChildren: () => import('./movies/movies-details/movies-details.module').then((m)=> m.MoviesDetailsPageModule),
+      }
+    ]
+  },
+  {
+    path: "newMovie",
+    loadChildren: () =>
+      import("./movies/movie-add/movie-add.module").then((m) => m.MovieAddPageModule),
+  },
+  {
+    path: 'formularios',
+    children: [
+      {
+        path: '',
+        loadChildren: () => import('./formularios/formularios.module').then( m => m.FormulariosPageModule)
+      },
+      {
+        path: 'importar',
+        loadChildren: () => import('./formularios/importar/importar.module').then( m => m.ImportarPageModule)
+      },
+      {
+        path: 'agregar',
+        loadChildren: () => import('./formularios/agregar/agregar.module').then( m => m.AgregarPageModule)
+      },
+      {
+        path: 'exportar',
+        loadChildren: () => import('./formularios/exportar/exportar.module').then( m => m.ExportarPageModule)
+      },
+    ]
+  },
+  
 ];
 
 @NgModule({
